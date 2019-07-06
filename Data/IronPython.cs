7bf5c5736b2b7fef677c67608d487de824dabaac
Toma Kolev <S0207933@fecolleges.london>
2019-07-06 11:23:10 +0100

    .

--- Data/IronPython.cs
+++ Data/IronPython.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 using Microsoft.Scripting;
 using Microsoft.Scripting.Hosting;
+using IronPython.Hosting;
+using IronPython.Modules;
 
 namespace EyeSPARC.Data
 {
     public static class IronPython
     {
+        public static ScriptEngine ScriptEngine;
 
+        public static IronPythonStatus EngineStatus = IronPythonStatus.Unknown;
+
+        public static void Initialize()
+        {
+            ScriptEngine = Python.CreateEngine();
+        }
+    }
+    public enum IronPythonStatus
+    {
+        Initialized,
+        Failed,
+        Unknown
     }
 }
