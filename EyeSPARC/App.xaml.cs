1bf0737a9e9ae8e3960d488e3121c6be7d2c9672
Toma Kolev <S0207933@fecolleges.london>
2019-07-17 19:00:27 +0100

    refactoring

--- EyeSPARC/App.xaml.cs
+++ EyeSPARC/App.xaml.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
